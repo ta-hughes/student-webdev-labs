e65dd2eef47130008aaf76681c113325b66d9636
Taylor Hughes <tahughes@pdx.edu>
2024-05-15 15:56:00 -0700

    Used formatting and experimented with different for-loops

--- 07-javascript/02-format.js
+++ 07-javascript/02-format.js
@@ -3,8 +3,17 @@ const formatItems = (input) => {
 
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+  //for (let i = 0; i < input.length; i++){
+  //  console.log(input[i]);
+  //}
 
-  return input;
+  let results = [];
+  for(let item of input){
+    //console.log(item.toFixed(3));
+    results.push(item.toFixed(3));
+  }
+
+  return results.join(', ');
 };
 
 console.log(formatItems([1, 4, 17, 26, 41]));
